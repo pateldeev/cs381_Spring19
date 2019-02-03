319f687ff313e4c4064397a2ceaacae0755543ed
pateldeev <42748234+pateldeev@users.noreply.github.com>
2019-02-02 19:03:57 -0800

    Add files via upload

--- AS00/src/TutorialApplication.cpp
+++ AS00/src/TutorialApplication.cpp
@@ -30,34 +30,38 @@ TutorialApplication::~TutorialApplication(void) {
 
 //---------------------------------------------------------------------------
 void TutorialApplication::createScene(void) {
-	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
-
-	mCamera->setPosition(0, 47, 222);
-
-	Ogre::Light *light = mSceneMgr->createLight("MainLight");
+	//for lighting
+	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5)); //global light level
+	Ogre::Light *light = mSceneMgr->createLight("MainLight"); //create basic light
 	light->setPosition(20.0, 80.0, 50.0);
 
+	mCamera->setPosition(0, 47, 222); //set camera position
+
+	//add ogre 1
 	Ogre::Entity *ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
 	Ogre::SceneNode *ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
 	ogreNode->attachObject(ogreEntity);
-	ogreNode->showBoundingBox(true);
+	ogreNode->showBoundingBox(true); //show aabb bounding box around model
 
+	//add ogre 2
 	Ogre::Entity *ogreEntity2 = mSceneMgr->createEntity("ogrehead.mesh");
 	Ogre::SceneNode *ogreNode2 = mSceneMgr->getRootSceneNode()->createChildSceneNode(Ogre::Vector3(84, 48, 0));
 	ogreNode2->attachObject(ogreEntity2);
-	ogreNode2->showBoundingBox(true);
+	ogreNode2->showBoundingBox(true); //show aabb bounding box around model
 
+	//add ogre 3
 	Ogre::Entity *ogreEntity3 = mSceneMgr->createEntity("ogrehead.mesh");
 	Ogre::SceneNode *ogreNode3 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
 	ogreNode3->setPosition(Ogre::Vector3(0, 104, 0));
-	ogreNode3->setScale(2, 1.2, 1);
+	ogreNode3->setScale(2, 1.2, 1); //scale model
 	ogreNode3->attachObject(ogreEntity3);
-	ogreNode3->showBoundingBox(true);
+	ogreNode3->showBoundingBox(true); //show aabb bounding box around model
 
+	//add ogre 4
 	Ogre::Entity *ogreEntity4 = mSceneMgr->createEntity("ogrehead.mesh");
 	Ogre::SceneNode *ogreNode4 = mSceneMgr->getRootSceneNode()->createChildSceneNode();
 	ogreNode4->setPosition(-84, 48, 0);
-	ogreNode4->roll(Ogre::Degree(-90));
+	ogreNode4->roll(Ogre::Degree(-90)); //rotate model
 	ogreNode4->attachObject(ogreEntity4);
-	ogreNode4->showBoundingBox(true);
+	ogreNode4->showBoundingBox(true); //show aabb bounding box around model
 }
